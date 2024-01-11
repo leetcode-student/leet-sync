d1f062613033a42ccc5fddd9132fd1e36eeafd09
leetcode-student <155063750+leetcode-student@users.noreply.github.com>
2024-01-10 23:03:16 -0800

    Time: 0 ms (100.00%) | Memory: 41.6 MB (7.34%) - LeetSync

---
+++ 339-nested-list-weight-sum/nested-list-weight-sum.java
@@ -0,0 +1,47 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        return depthSum(nestedList, 1);   
+    }
+
+    public int depthSum(List<NestedInteger> nestedList, int currDepth) {
+        int depthSum = 0;
+        
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                depthSum += currDepth * nestedInteger.getInteger();
+            } else {
+                depthSum += depthSum(nestedInteger.getList(), currDepth + 1);
+            }
+        }
+
+        return depthSum;
+    }
+}
