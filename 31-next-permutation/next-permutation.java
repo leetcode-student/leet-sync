78d2e99f34008de885882f4b65048edd61caf79c
leetcode-student <155063750+leetcode-student@users.noreply.github.com>
2024-01-25 17:14:40 -0800

    Time: 9 ms (6.08%) | Memory: 43.4 MB (6.61%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,56 +1,54 @@
 /**
 
-For sub array, permutation isn't possible if nums[i] > nums[j] for all i > j
+1,2,3,4,5
+nextperm=2,1,3,4,5
 
-1,2,3
-1,3,2
-2,1,3
-2,3,1
-3,1,2
-3,2,1
+5,4,3,2,1
+nextperm=none
+-> reset to 1,2,3,4,5
 
-1,3,2,1
-2,1,2,3
-
-3,2,1
-
-7,2,8,5,1
-7,8,5,2,1
+find the last element such at an higher element exists to its right
+swap it with the smallest element to the right that's larger (right most)
+right side will be descending... reverse it
 
 **/
 class Solution {
     public void nextPermutation(int[] nums) {
-        if (nums.length == 1) {
-            return;
-        }
+        int highestIdx = nums.length - 1;
+        int firstIdx = -1;
 
-        int nextDecreasingIdx = -1;
         for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                nextDecreasingIdx = i;
+            if (nums[i] < nums[highestIdx]) {
+                firstIdx = i;
                 break;
+            } else if (nums[i] > nums[highestIdx]) {
+                highestIdx = i;
             }
         }
 
-        System.out.println("nextDecreasingIdx=" + nextDecreasingIdx);
+        System.out.println("firstIdx=" + firstIdx);
 
-        if (nextDecreasingIdx != -1) {
-            int nextHighestIdx = nextDecreasingIdx + 1;
-            for (int i = nextHighestIdx + 1; i < nums.length; i++) {
-                if (nums[i] > nums[nextDecreasingIdx] && nums[i] <= nums[nextHighestIdx]) {
-                    nextHighestIdx = i;
+        if (firstIdx != -1) {
+            int nextHighest = -1;
+
+            for (int i = firstIdx + 1; i < nums.length; i++) {
+                if (nums[i] > nums[firstIdx] && (nextHighest == -1 || nums[i] <= nums[nextHighest])) {
+                    nextHighest = i;
                 }
             }
 
-            System.out.println("nextHighestIdx=" + nextHighestIdx);
+            System.out.println("nextHighest=" + nextHighest);
 
-            swap(nums, nextDecreasingIdx, nextHighestIdx);
+            swap(nums, firstIdx, nextHighest);
         }
 
-        reverse(nums, nextDecreasingIdx + 1, nums.length - 1);
+        reverse(nums, firstIdx + 1, nums.length - 1);
     }
 
-    private void reverse(int[] nums, int i, int j) {
+    private void reverse(int[] nums, int start, int end) {
+        int i = start;
+        int j = end;
+
         while (i < j) {
             swap(nums, i, j);
             i++;
@@ -58,9 +56,9 @@ class Solution {
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
