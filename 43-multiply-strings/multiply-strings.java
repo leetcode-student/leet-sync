a2ed236f0403352a3601a3e0161d705540249a20
leetcode-student <155063750+leetcode-student@users.noreply.github.com>
2024-01-27 22:46:18 -0800

    Time: 83 ms (5.32%) | Memory: 46.1 MB (5.02%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,92 +1,128 @@
 /**
- 
-12
- 5
-__
-10
-50
 
+multiply
 
- 5
-12
-__
-10
-50
+123
+456
+
+123 * 6
+123 * 5 * 10
+123 * 4 * 100
+
+123
+  6
+
+3 * 6 -> 8, carry 1
+2 * 6 + 1 -> 3, carry 1
+1 * 6 + 1 -> 7
+
+add
+
+123
+456
+
+3 + 6
+2 + 5
+1 + 4
 
 **/
 class Solution {
     public String multiply(String num1, String num2) {
-        StringBuilder total = new StringBuilder("0");
+        String result = "0";
 
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int carry = 0;
-            StringBuilder subTotalReversed = new StringBuilder();
-            for (int j = num1.length() - 1; j > i; j--) {
-                subTotalReversed.append("0");
-            }
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                int digit1 = Character.getNumericValue(num1.charAt(i));
-                int digit2 = Character.getNumericValue(num2.charAt(j));
+        for (int i = num2.length() - 1; i >= 0; i--) {
+            String product = multiply(num1, Character.getNumericValue(num2.charAt(i)));
+            product = multiplyTens(product, num2.length() - 1 - i);
 
-                int digitProduct = digit1 * digit2 + carry;
-
-                System.out.println(digit1 + " * " + digit2 + " + " + carry + " = " + digitProduct);
-
-                carry = digitProduct / 10;
-                digitProduct = digitProduct % 10;
-
-                subTotalReversed.append(digitProduct);
-            }
-
-            if (carry > 0) {
-                subTotalReversed.append(carry);
-            }
-
-            total = new StringBuilder(add(total.toString(), subTotalReversed.reverse().toString()));
-            System.out.println("total=" + total);
+            result = add(result, product);
         }
 
-        int firstIdx = 0;
-        while (firstIdx + 1 < total.length() && total.charAt(firstIdx) == '0') {
-            firstIdx++;
+        return clearLeadingZeroes(result);
+    }
+
+    private String clearLeadingZeroes(String num) {
+        int firstNonZero = num.length() - 1;
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) != '0') {
+                firstNonZero = i;
+                break;
+            }
         }
 
-        return total.toString().substring(firstIdx);
-    } 
+        return num.substring(firstNonZero);
+    }
 
-    private String add(String num1, String num2) {
-        String longer = num1.length() >= num2.length() ? num1 : num2;
-        String shorter = num1.length() < num2.length() ? num1 : num2;
-
-        if (longer.equals("0")) {
-            return shorter;
-        } else if (shorter.equals("0")) {
-            return longer;
+    private String multiplyTens(String num, int count) {
+        StringBuilder result = new StringBuilder(num);
+        for (int i = 0; i < count; i++) {
+            result.append("0");
         }
+        System.out.println(num + " -> " + result);
+        return result.toString();
+    }
 
+    public String multiply(String num1, int digit2) {
+        StringBuilder result = new StringBuilder();
         int carry = 0;
-        StringBuilder addedReversed = new StringBuilder();
-        for (int i = longer.length() - 1; i >= 0; i--) {
-            int digit1 = Character.getNumericValue(longer.charAt(i));
-            int shorterIndex = i + shorter.length() - longer.length();
-            int digit2 = shorterIndex >= 0 ? Character.getNumericValue(shorter.charAt(shorterIndex)) : 0;
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int digit1 = Character.getNumericValue(num1.charAt(i));
+            int product = digit1 * digit2 + carry;
 
-            int digitSum = digit1 + digit2 + carry;
+            carry = product / 10;
+            product = product % 10;
 
-            carry = digitSum / 10;
-            digitSum = digitSum % 10;
+            //System.out.println("digit1=" + digit1 + ", digit2=" + digit2 + ", product=" + product);
 
-            addedReversed.append(Integer.toString(digitSum));
+            result.append(product);
         }
 
         if (carry > 0) {
-            addedReversed.append(Integer.toString(carry));
+            result.append(carry);
         }
 
-        StringBuilder added = addedReversed.reverse();
+        result.reverse();
+        
+        if (result.isEmpty()) {
+            return "0";
+        }
 
-        System.out.println(num1 + " + " + num2 + " = " + added);
+        System.out.println(num1 + " * " + digit2 + " = " + result);
+        
+        return result.toString();
+    }
 
-        return added.toString();
+    public String add(String num1, String num2) {
+        if (num1.length() < num2.length()) {
+            return add(num2, num1);
+        }
+
+        int lengthDiff = num1.length() - num2.length();
+
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int digit1 = Character.getNumericValue(num1.charAt(i));
+            int digit2 = i - lengthDiff >= 0 ? Character.getNumericValue(num2.charAt(i - lengthDiff)) : 0;
+            int sum = digit1 + digit2 + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            result.append(sum);
+        }
+
+        if (carry > 0) {
+            result.append(carry);
+        }
+
+        result.reverse();
+        
+        if (result.isEmpty()) {
+            result.append("0");
+        }
+
+        System.out.println(num1 + " + " + num2 + " = " + result);
+        
+        return result.toString();
     }
 }
