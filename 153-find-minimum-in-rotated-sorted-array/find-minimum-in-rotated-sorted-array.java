c7744907f73e8e8483e0819b389928430b6452ac
leetcode-student <155063750+leetcode-student@users.noreply.github.com>
2024-01-28 20:35:13 -0800

    Time: 0 ms (100.00%) | Memory: 42.2 MB (11.93%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -5,19 +5,23 @@ if nums[start] <= nums[end], then entire array is sorted, pick the lowest
 */
 class Solution {
     public int findMin(int[] nums) {
-        return findMin(nums, 0, nums.length - 1);
-    }
+        int start = 0;
+        int end = nums.length - 1;
 
-    private int findMin(int[] nums, int start, int end) {
-        
-        int mid = start + (end - start) / 2;
+        while (start < end) {
+            if (nums[start] <= nums[end]) {
+                return nums[start];
+            }
 
-        if (nums[start] <= nums[end]) {
-            return nums[start];
-        } else if (nums[mid] > nums[end]) {
-            return findMin(nums, mid + 1, end);
-        } else {
-            return findMin(nums, start, mid);
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
         }
+
+        return nums[start];
     }
 }
